28dc867bd46cb5c3432a35d1c2dd51ecaba219a9
Ivan Petrovic <ivan.petrovic@ingsoftware.com>
2022-08-05 09:36:04 +0200

    Angular angular-max-recipe_shopping_list: Adding Navigation, Passing Data with Event and Property Binding, Add Ingredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,15 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
+      'This is a test',
+      'https://assets.cntraveller.in/photos/6164302c536a64b77fac804f/master/pass/navratri-meals-lead.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
       'This is a test',
       'https://assets.cntraveller.in/photos/6164302c536a64b77fac804f/master/pass/navratri-meals-lead.jpg'
     ),
@@ -18,4 +24,8 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
