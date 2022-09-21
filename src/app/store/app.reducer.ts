853851fea19ec5791d0c7b46bd60b6e73b8b5a0c
Ivan Petrovic <ivan.petrovic@ingsoftware.com>
2022-09-21 10:21:02 +0200

    use Store Devtools (@ngrx/store-devtools), use Router Store (@ngrx/router-store)

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,4 +1,5 @@
 import { ActionReducerMap } from '@ngrx/store';
+import * as fromRouter from '@ngrx/router-store';
 
 import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
 import * as fromAuth from '../auth/store/auth.reducer';
@@ -6,9 +7,11 @@ import * as fromAuth from '../auth/store/auth.reducer';
 export interface AppState {
   shoppingList: fromShoppingList.State;
   auth: fromAuth.State;
+  router: fromRouter.RouterReducerState;
 }
 
 export const appReducer: ActionReducerMap<AppState> = {
   shoppingList: fromShoppingList.shoppingListReducer,
   auth: fromAuth.authReducer,
+  router: fromRouter.routerReducer,
 };
